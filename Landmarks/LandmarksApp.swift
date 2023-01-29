4fb7a099f37f9c26d1ced661404e082d29d4d184
Zach Peterson <zachpeterson13@icloud.com>
2023-01-28 22:30:36 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Zach Peterson on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
