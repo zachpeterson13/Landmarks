3e6c20b4dcff1c536cb768c0d0460d389c91c39b
Zach Peterson <zachpeterson13@icloud.com>
2023-01-29 00:07:04 -0700

    Completed building lists and navigation tutorial

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Zach Peterson on 1/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
